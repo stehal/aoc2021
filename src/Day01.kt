26f0a9c56cc8c8ae363afd49cfe741d7352a9274
Steven Hale <steven.hale@lm.se>
2021-12-01 10:02:25 +0100

    day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,24 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var count = 0;
+        input.map(String::toInt).zipWithNext().forEach { if (it.first < it.second) count++ }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var count = 0;
+        val numbers = input.map(String::toInt)
+        numbers.zip(numbers.subList(1, numbers.lastIndex + 1)).zip(numbers.subList(2, numbers.lastIndex + 1))
+            .map { it.first.first + it.first.second + it.second }.zipWithNext()
+            .forEach { if (it.first < it.second) count++ }
+        return count
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("part 1 ${part1(input)}")
+    println("part 2 ${part2(input)}")
 }
