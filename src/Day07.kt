7027df48c6009e4e59b5e853e2628ae71545d487
Steven Hale <steven.hale@lm.se>
2021-12-07 12:07:21 +0100

    day7

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,23 +1,41 @@
+import kotlin.math.abs
 
-
-
+data class Result(val candidate: Int, val distance: Int)
 
 fun main() {
 
-
     fun part1(input: List<String>): Int {
-        return input.size
+        val positions = input.first().split(",").map { it.toInt() }
+
+        return (positions.minOf { it }..positions.maxOf { it }).map { candidate ->
+            Result(
+                candidate,
+                positions.sumOf { abs(candidate - it) }
+            )
+        }
+            .minOf { it.distance }
+    }
+
+    fun fuel(start: Int, finish: Int): Int {
+        val dist = abs(start - finish)
+        return dist * (dist + 1) / 2
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val positions = input.first().split(",").map { it.toInt() }
+        return (positions.minOf { it }..positions.maxOf { it }).map { candidate ->
+            Result(
+                candidate,
+                positions.sumOf { fuel(candidate, it) }
+            )
+        }.minOf { it.distance }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day06_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day06")
+    val testInput = readInput("Day07_test")
+    check(part1(testInput) == 37)
+    check(part2(testInput) == 168)
+    val input = readInput("Day07")
     println("part 1 ${part1(input)}")
     println("part 2 ${part2(input)}")
 }
